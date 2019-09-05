b6851973a0a756b147a27cdbaee8c9bc675ad102
Dannas Cornell <dannas.cornell@gmail.com>
2019-09-05 11:19:55 -0500

    Locale files was added
    Locale files was added

---
+++ introduction/locale.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <locale.h>
+int main(){
+ setlocale(LC_CTYPE,"Spanish");
+ printf("Niños");
+ return 0;
+}
---
+++ introduction/locale_ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+ 
+ printf("ni%cos \n",164);//ñ
+ printf("ni%cos",165);//Ñ 
+ return 0;
+}
