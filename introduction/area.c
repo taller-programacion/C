3c9b919a4066b3ec838d8985dca4afec1f38dae6
JohnVazquez1 <72407073+JohnVazquez1@users.noreply.github.com>
2020-10-06 22:46:06 -0500

    Actualización final de clase
    Se actualizó con los comentarios finales de la clase.

--- introduction/area.c
+++ introduction/area.c
@@ -3,8 +3,8 @@
 #define PI 3.1416 //Definiendo una constante
 
 int main(){ //Se indica el int para que espere un entero
-	float area, radio;
-	radio = 5;
+	float area, radio; //Usando la palabra reservada de un flotante con las variables
+	radio = 5; //Se indica el valor de radio
 	area = PI * pow(5,2);
 	printf("Area\n");
 	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
