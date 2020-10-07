6505f4d8e4d93d0091bf5d2960e8a186cd71fe1f
JohnVazquez1 <72407073+JohnVazquez1@users.noreply.github.com>
2020-10-06 22:33:16 -0500

    Actualización de archivo área
    Se modificó y comentó.

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-#include<math.h>
-#define PI 3.1416
+#include<stdio.h> //Librería de entrada y salida
+#include<math.h> //Librería de matemática
+#define PI 3.1416 //Definiendo una constante
 
-int main(){
+int main(){ //Se indica el int para que espere un entero
 	float area, radio;
 	radio = 5;
 	area = PI * pow(5,2);
 	printf("Area\n");
 	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
+	return 0;
 }
