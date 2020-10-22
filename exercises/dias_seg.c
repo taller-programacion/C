8273ed80df2a35fdb3cca3e171eab1ca8409d2df
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-22 15:00:00 -0500

    Dias_seg.c was updated

--- exercises/dias_seg.c
+++ exercises/dias_seg.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    //Declaración de variables
     int dias;
     float resultado;
-    
+
+    //Intrucciones para el usuario
+    printf("Ingresa un número de días para calcular los segundos que tiene: ");
+    //Leemos la variable dias
+    scanf("%d", &dias);
     resultado = dias * (24 * 60 * 60);
-    printf("En %d días hay %f segundos.", dias, resultado);
+    printf("\tEn %d días hay %0.2f segundos.\n", dias, resultado);
     
     return 0;
 }
