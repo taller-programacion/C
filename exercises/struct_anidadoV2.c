3deff8f81bdba739d125a0de017ea445ba25ae60
Dannas Cornell <dannas.cornell@gmail.com>
2021-11-29 14:42:45 -0600

    🎨  Source file updated

--- exercises/struct_anidadoV2.c
+++ exercises/struct_anidadoV2.c
@@ -16,7 +16,7 @@ struct info{
 struct academico{
 	char numeroEmpleado[13];
 	struct info datos_profesor;
-}profesor;
+}profesor[5];
 
 struct alumnado{
 	char numeroCuenta[13];
@@ -25,32 +25,37 @@ struct alumnado{
 }estudiante;
 
 int main(void){
-	int op;
+	int op, reg;
 
+do{
 	puts("¿Qué deseas capturar?");
 	puts("1.-Profesorado");
 	puts("2.-Alumnado");
+	puts("3.-Salir");
 
 	scanf("%i", &op);
-
 	if(op == 1){
 		getchar();
-		printf("Dame número empleado:");
-		fflush(stdin);
-		fgets(profesor.numeroEmpleado,13,stdin);
-		printf("Dame nombre: ");
-		fflush(stdin);
-		fgets(profesor.datos_profesor.nombre, 25, stdin);
-		printf("Dame apellido paterno: ");
-		fflush(stdin);
-		fgets(profesor.datos_profesor.apPaterno, 25, stdin);
-		printf("Dame apellido materno: ");
-		fflush(stdin);
-		fgets(profesor.datos_profesor.apMaterno, 25, stdin);
-		printf("\n\tID: %s \n\tNombre:%s %s %s", profesor.numeroEmpleado, profesor.datos_profesor.nombre, profesor.datos_profesor.apPaterno, profesor.datos_profesor.apMaterno);   
+		puts("¿Cuántos registros deseas?");
+		scanf("%i", &reg);
+		for (int i=0; i<reg; i++){
+			printf("Dame número empleado:");
+			fflush(stdin);
+			fgets(profesor[i].numeroEmpleado,13,stdin);
+			printf("Dame nombre: ");
+			fflush(stdin);
+			fgets(profesor[i].datos_profesor.nombre, 25, stdin);
+			printf("Dame apellido paterno: ");
+			fflush(stdin);
+			fgets(profesor[i].datos_profesor.apPaterno, 25, stdin);
+			printf("Dame apellido materno: ");
+			fflush(stdin);
+			fgets(profesor[i].datos_profesor.apMaterno, 25, stdin);
+		//printf("\n\tID: %s \tNombre:%s\tApellido Paterno: %s\tApellido Materno: %s", profesor.numeroEmpleado, profesor.datos_profesor.nombre, profesor.datos_profesor.apPaterno, profesor.datos_profesor.apMaterno);
+		} //Fin de For
 	}else if(op == 2){
 		getchar();
-		printf("Dame número empleado: ");
+		printf("Dame número de estudiate: ");
 		fflush(stdin);
 		fgets(estudiante.numeroCuenta,13,stdin);
 		printf("Dame nombre: ");
@@ -62,10 +67,17 @@ int main(void){
 		printf("Dame apellido materno: ");
 		fflush(stdin);
 		fgets(estudiante.datos_estudiante.apMaterno, 25, stdin);
-		printf("\n\tID: %s \n\tNombre:%s %s %s", estudiante.numeroCuenta, estudiante.datos_estudiante.nombre, estudiante.datos_estudiante.apPaterno, estudiante.datos_estudiante.apMaterno);   
+		printf("\n\tID: %s \n\tNombre:%s\n\tApellido Paterno: %s\n\tApellido Materno: %s", estudiante.numeroCuenta, estudiante.datos_estudiante.nombre, estudiante.datos_estudiante.apPaterno, estudiante.datos_estudiante.apMaterno);   
 
 	}else{
 		printf("Dame una opción válida");
 	}
+}while (op != 3);
+	puts("----------------------------");
+	puts("Datos Profesores:");
+	for (int i=0; i<reg; i++){
+	    printf("\n\tID: %s \tNombre:%s\tApellido Paterno: %s\tApellido Materno: %s", profesor[i].numeroEmpleado, profesor[i].datos_profesor.nombre, profesor[i].datos_profesor.apPaterno, profesor[i].datos_profesor.apMaterno);
+	}
+	puts("----------------------------");
 	return 0;
 }
