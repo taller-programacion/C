355f873ecf4a3289778d0ef7a24b1952291c2066
Dannas Cornell <dannas.cornell@gmail.com>
2022-02-24 15:54:48 -0600

    Create entero.c
    Se creo codigo de ejemplo de enteros

---
+++ introduction/entero.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//Declaracion de variable
+int progra;
+
+//Declaracion y asignacion de variable
+int pepe = 1000;
+
+int main(){
+    progra = 64; //Asignacion
+    /*printf("La variable progra vale: %i\n", progra);
+    printf("\tPepe, vales %i", pepe);
+
+    pepe = 0; //Asignando nuevo valor a la variable
+    printf("\tPepe, ahora vales %i", pepe);
+    */
+    printf("\v");
+    printf("La variable progra vale: %i y %c", progra, progra);
+    printf("\n");
+
+    return 0;
+}
