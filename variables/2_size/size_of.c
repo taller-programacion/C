32dfcfb2b59e7e38d7edae3a10a755b9ca293fbb
Dannas Cornell <dannas.cornell@gmail.com>
2022-03-24 14:39:36 -0600

    Update size_of.c
    Cambios

--- variables/2_size/size_of.c
+++ variables/2_size/size_of.c
@@ -9,10 +9,11 @@ int main(void){
     printf("====\t===============\n");
     
     printf("char\t%ld\n",sizeof(char)); //¿De qué tamaño es un char?
-    printf("string\t%ld\n",sizeof(char[50])); //¿De qué tamaño es un char de 50 espacios (strings)?
+    printf("string\t%ld\n",sizeof(char[10])); //¿De qué tamaño es un char de 50 espacios (strings)?
     printf("int\t%ld\n",sizeof(int)); //¿De qué tamaño es un entero?
     printf("float\t%ld\n",sizeof(float)); //¿De qué tamaño es un flotante?
     printf("double\t%ld\n",sizeof(double)); //¿De qué tamaño es un flotante double?
-    
+    printf("double\t%ld\n",sizeof(double)); //¿De qué tamaño es un flotante double?
+
     return 0;
 }
